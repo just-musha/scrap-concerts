f044d47f0af0f49cec13e753a66723c1bc3c2f97
Maria Bulatova <maria.r.bulatova@yandex.ru>
2020-03-13 16:42:15 +0300

    Initial commit - generated scrapy project

---
+++ scrap_concerts/items.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class ScrapConcertsItem(scrapy.Item):
+    date = scrapy.Field()
+    name = scrapy.Field()
+    description = scrapy.Field()
+    picture_link = scrapy.Field()
